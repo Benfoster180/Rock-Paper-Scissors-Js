8a98d3c12c507d792735b9cca834f7ae905b9966
Cursed <31930426+Benfoster180@users.noreply.github.com>
2022-08-12 19:49:39 +0100

    Improved the code

--- Rock Papper Scissors/script.js
+++ Rock Papper Scissors/script.js
@@ -1,121 +1,100 @@
 
 Player = "N/A"
 Bot = "N/A"
-Game_end = false
-function Papper() {
-    Game_end = false
-    Bot_move()
+
+
+//Function that is linked to the papper button
+function Papper_Button_Clicked_Event() {
+    Generate_Bots_Move()
     return Player = "Papper"
 }
 
-function Scissors() {
-    Bot_move()
-    return Player = "Scissors"
+//Function that is linked to the Scissors button
+function Scissors_Button_Clicked_Event() {
+    Generate_Bots_Move()
+    return Player = "Scissors";;
 }
 
-function Rock() {
-    Bot_move()
-    return Player = "Rock"
+//Function that is linked to the Rock button
+function Rock_Button_Clicked_Event() {
+    Generate_Bots_Move()
+    return Player = "Rock";
 }
 
-function Bot_move() {
-        var answers = [ "Rock",
-    "Papper",
-    "Scissors"];
-
-    var index = Math.floor(Math.random() * answers.length);
-    
-
-    Bot=answers[index]
-    Result()
-    return Bot
+function Generate_Bots_Move() {
+    //Assigns the Bot a random move!
+        var Possible_Moves = [ "Rock","Papper","Scissors"];
+        var index = Math.floor(Math.random() * Possible_Moves.length);
+        Bot=Possible_Moves[index]
+        Calculate_Winner()
+        return;
 }
 
 
 
-function Result() {
+function Calculate_Winner() {
     
-    //If Draw
-    if (Player==Bot && Game_end == false){
-        Who_won = "Draw"
-        Current_draw_score = document.getElementById('Draw').innerHTML;
-        New_Draw = Number(Current_draw_score) + 1;
-        document.getElementById('Draw').innerHTML = New_Draw;
-        last_game()
-        Game_end = true
-    
+    // Used to Detect any Draw
+    if (Player==Bot){
+        //Used to Display who won the game!
+        Game_Result = "Draw"
+        //Updates the Scoreboad, Used to control Draw event!
+        document.getElementById('Number_of_Draws').innerHTML = Number(document.getElementById('Number_of_Draws').innerHTML) + 1;
+        Update_Game_Data()
+        return;
+       
     }
     
     //Player Win by Rock
     if (Player=="Rock" && Bot=="Scissors"){
-        Who_won = "Player"
-        Current_win_score = document.getElementById('Win').innerHTML;
-        New_Win = Number(Current_win_score) + 1;
-        document.getElementById('Win').innerHTML = New_Win;
-        last_game()
+        //Used to Display who won the game!
+        Game_Result = "Player"
+        //Updates the Scoreboad, Used to control Win event!
+        document.getElementById('Number_of_Wins').innerHTML = Number(document.getElementById('Number_of_Wins').innerHTML) + 1;
+        Update_Game_Data()
+        return;
        
     }
 
     if (Player=="Scissors" && Bot=="Papper"){
-        Who_won = "Player"
-        Current_win_score = document.getElementById('Win').innerHTML;
-        New_Win = Number(Current_win_score) + 1;
-        document.getElementById('Win').innerHTML = New_Win;
-        last_game()
+        Game_Result = "Player"
+        //Updates the Scoreboad, Used to control Win event!
+        document.getElementById('Number_of_Wins').innerHTML = Number(document.getElementById('Number_of_Wins').innerHTML) + 1;
+        Update_Game_Data()
+        return;
        
     }
 
     if (Player=="Papper" && Bot=="Rock"){
-        Who_won = "Player"
-        Current_win_score = document.getElementById('Win').innerHTML;
-        New_Win = Number(Current_win_score) + 1;
-        document.getElementById('Win').innerHTML = New_Win;
-        last_game()
+        Game_Result = "Player"
+        //Updates the Scoreboad, Used to control Win event!
+        document.getElementById('Number_of_Wins').innerHTML = Number(document.getElementById('Number_of_Wins').innerHTML) + 1;
+        Update_Game_Data()
+        return;
         
     }
+    
 
-    if (Bot=="Papper" && Player=="Rock"){
-        Who_won = "Bot"
-        Current_Lose_score = document.getElementById('Lose').innerHTML;
-        New_Lose = Number(Current_Lose_score) + 1;
-        document.getElementById('Lose').innerHTML = New_Lose;
-        last_game()   
-    }
-
-    if (Bot=="Scissors" && Player=="Papper"){
-        Who_won = "Bot"
-        Current_Lose_score = document.getElementById('Lose').innerHTML;
-        New_Lose = Number(Current_Lose_score) + 1;
-        document.getElementById('Lose').innerHTML = New_Lose;
-        last_game()   
-    }
-
-    if (Bot=="Rock" && Player=="Scissors"){
-        Who_won = "Bot"
-        Current_Lose_score = document.getElementById('Lose').innerHTML;
-        New_Lose = Number(Current_Lose_score) + 1;
-        document.getElementById('Lose').innerHTML = New_Lose;
-        last_game()   
+    else{
+        Game_Result = "Bot"
+        //Updates the Scoreboad, Used to control lose event!
+        document.getElementById('Number_of_Loses').innerHTML = Number(document.getElementById('Number_of_Loses').innerHTML) + 1;
+        Update_Game_Data()
+        return;
     }
 
 }
 
-function last_game(){
-    document.getElementById('You_Played').innerHTML = Player;
-    document.getElementById('Bot_move').innerHTML = Bot;
-    document.getElementById('game_result').innerHTML = Who_won ;
-
-    //Used to display the numbers of games that have been played
-    Play_game_counter = document.getElementById('play').innerHTML;
-    Played_games = Number(Play_game_counter) + 1;
-    document.getElementById('play').innerHTML = Played_games;
-
-    wins = document.getElementById('Win').innerHTML;
-    loses =document.getElementById('Lose').innerHTML;
-
-    Sum_all_games = Number(wins + loses);
-    Perctange = Number((wins / Sum_all_games) * 100) + "%" ;
-    console.log(Perctange);
-    document.getElementById('kd').innerHTML = Perctange;
+function Update_Game_Data(){
+    //The Move the player made
+    document.getElementById('What_did_the_player_use').innerHTML = Player;
+    //The Move the Bot made
+    document.getElementById('What_did_the_Bot_Use').innerHTML = Bot;
+    //Who One the game
+    document.getElementById('game_result').innerHTML = Game_Result ;
+    //Updates The Ammount of gamesplayed
+    document.getElementById('Number_of_Games_Played').innerHTML =  Number(document.getElementById('Number_of_Games_Played').innerHTML)+1;
+    //WorksOut the KD
+    document.getElementById('Win_to_loss_ratio').innerHTML = ((Number(document.getElementById('Number_of_Wins').innerHTML) / Number(document.getElementById('Number_of_Games_Played').innerHTML)) * 100).toFixed(2) +"%";
+      
 }
-
